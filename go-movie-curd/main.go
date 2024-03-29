9720ffba42ec1f6d250621b696f3daaf821cc482
omg12347 <omghumre74@gmail.com>
2024-03-30 01:01:48 +0530

    added two movies

--- go-movie-curd/main.go
+++ go-movie-curd/main.go
@@ -9,3 +9,33 @@ import (
 	"net/http"
 	"strconv"
 )
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID:"1",Isbn:"438227",Title:"Movie one",Director:*Director{Firstname: "om",Lastname: "ghumre"}})
+	movies = append(movies, Movie{ID:"2",Isbn: "767676",Title: "Movie two", Director: *Director{Firstname: "oo", Lastname: "hh"}})
+	
+	r.HandleFunc("/movies"getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}",updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}",deleteMovie).Methods("DELETE")
+
+	fmt.Println("Starting port at 80000")
+	log.Fatal(http.ListenAndServe(":8000",r))
+}
